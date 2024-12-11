81ce303b82e4eb83da233168ef1c994884ef6256
Rysndavjd <rysndavjd@proton.me>
2024-12-11 19:30:43 +0200

    set absolute brightness in percent working

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use std::path::Path;
 use std::num::{ParseFloatError, ParseIntError};
+use std::io::{Error, Write, ErrorKind};
 
 fn get_brightness(device: &str) -> Result<f32, String> {
     let brightness = fs::read_to_string(format!("{}/brightness", device))
@@ -20,6 +21,28 @@ fn get_max_brightness(device: &str) -> Result<u32, String> {
 
 }
 
+fn set_brightness_absolute_percent(device: &str, percentage: &u32) {
+    let max_brightness: u32 = get_max_brightness(device).unwrap();
+    let new_brightness: u32 = ( percentage / 100 ) * max_brightness;
+    println!("new_brightness: {new_brightness}");
+    
+    let brightness: Result<fs::File, Error> = fs::OpenOptions::new()
+        .read(false)
+        .write(true)
+        .create(false)
+        .open(format!("{device}/brightness"));
+    
+    match brightness {
+        Ok(mut file) => {
+            file.write_all(new_brightness.to_string().as_bytes()).expect("Unable to write brightness");
+        },
+        Err(e) => match e.kind() {
+            ErrorKind::NotFound => eprintln!("The file was not found."),
+            ErrorKind::PermissionDenied => eprintln!("Permission denied."),
+            e => eprintln!("An error occurred: {}", e),
+        }
+    }
+}
 
 fn check_backlight(backlight: &str) -> Result<String, String> {
     let class: &Path = Path::new(&backlight);
@@ -43,7 +66,7 @@ fn main() {
         Ok(brightness) => println!("Max brightness is: {}", brightness),
         Err(e) => eprintln!("Error: {}", e),
     }*/
-
+    set_brightness_absolute_percent("/sys/class/backlight/apple-panel-bl/", &100);
     match check_backlight("apple-panel-bl") {
         Ok(o) => println!("works: {}", o),
         Err(e) => eprintln!("error: {}", e),
