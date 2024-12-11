d813072434b2be71f45b66924ffd172661e42dd4
Rysndavjd <rysndavjd@proton.me>
2024-12-11 23:42:31 +0200

    fixed small stuff, next messing with cmdline arguments

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::fs;
 use std::path::Path;
 use std::num::{ParseFloatError, ParseIntError};
 use std::io::{Error, Write, ErrorKind};
+use std::env;
 
 fn get_brightness(device: &str) -> Result<f32, String> {
     let brightness = fs::read_to_string(format!("{}/brightness", device))
@@ -21,11 +22,9 @@ fn get_max_brightness(device: &str) -> Result<u32, String> {
 
 }
 
-fn set_brightness_absolute_percent(device: &str, percentage: &u32) {
+fn set_brightness_absolute_percent(device: &str, percentage: &u8) {
     let max_brightness: u32 = get_max_brightness(device).unwrap();
-    let new_brightness: u32 = ( percentage / 100 ) * max_brightness;
-    println!("new_brightness: {new_brightness}");
-    
+    let new_brightness: f32 = ( *percentage as f32 / 100.0 ) * *&max_brightness as f32;
     let brightness: Result<fs::File, Error> = fs::OpenOptions::new()
         .read(false)
         .write(true)
@@ -34,7 +33,7 @@ fn set_brightness_absolute_percent(device: &str, percentage: &u32) {
     
     match brightness {
         Ok(mut file) => {
-            file.write_all(new_brightness.to_string().as_bytes()).expect("Unable to write brightness");
+            file.write_all(new_brightness.round().to_string().as_bytes()).expect("Unable to write brightness.");
         },
         Err(e) => match e.kind() {
             ErrorKind::NotFound => eprintln!("The file was not found."),
@@ -60,15 +59,17 @@ fn check_backlight(backlight: &str) -> Result<String, String> {
     }
 }
 
-fn main() {
-    /* 
-    match get_max_brightness("/sys/class/backlight/apple-panel-bl/") {
-        Ok(brightness) => println!("Max brightness is: {}", brightness),
-        Err(e) => eprintln!("Error: {}", e),
-    }*/
-    set_brightness_absolute_percent("/sys/class/backlight/apple-panel-bl/", &100);
-    match check_backlight("apple-panel-bl") {
-        Ok(o) => println!("works: {}", o),
-        Err(e) => eprintln!("error: {}", e),
-    }
+fn main() {    
+    let args: Vec<String> = env::args().collect();
+    
+    //match check_backlight("apple-panel-bl") {
+    //    Ok(backlight) => {
+    //        //if percentage > &100 {
+    //        //    eprintln!("Cannot set display brightness higher then 100%.");
+    //        //    percentage = &100;
+    //        //}
+    //        set_brightness_absolute_percent(&backlight, &100);
+    //    },
+    //    Err(e) => eprintln!("Error: {}", e),
+    //}
 }
