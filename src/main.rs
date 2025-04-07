98db1f05795896018a3399f2de06c429cf6464cb
rysndavjd <rysndavjd@unknown.me>
2025-04-07 21:43:57 +0200

    doing parsing

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,74 @@
-use std::fs;
+use std::{fs::File, fs};
 use std::path::Path;
-use std::num::{ParseFloatError, ParseIntError};
-use std::io::{Error, Write, ErrorKind};
+use std::io::Write;
 use std::env;
-use std::process::exit;
+use clap::{Arg, command, ArgAction};
 
-fn list_devices() {
-    return;
-}
-
-fn get_brightness(device: &str) -> Result<f32, String> {
-    let brightness = fs::read_to_string(format!("{}/brightness", device))
-    .map_err(|err| format!("Reading brightness failed: {}", err))?;
-
-    brightness.trim().parse::<f32>()
-    .map_err(|err: ParseFloatError| format!("Failed to convert brightness to float: {}", err))
-
-}
-
-fn get_max_brightness(device: &str) -> Result<u32, String> {
-    let max_brightness = fs::read_to_string(format!("{}/max_brightness", device))
-        .map_err(|err| format!("Reading max brightness failed: {}", err))?;
-
-    max_brightness.trim().parse::<u32>()
-        .map_err(|err: ParseIntError| format!("Failed to convert max brightness to int: {}", err))
-
-}
-
-fn set_brightness_absolute_percent(device: &str, percentage: &u8) {
-    let max_brightness: u32 = get_max_brightness(device).unwrap();
-    let new_brightness: f32 = ( *percentage as f32 / 100.0 ) * *&max_brightness as f32;
-    let brightness: Result<fs::File, Error> = fs::OpenOptions::new()
-        .read(false)
-        .write(true)
-        .create(false)
-        .open(format!("{device}/brightness"));
+fn list_devices() -> Result<String, Box<dyn std::error::Error>> {
     
-    match brightness {
-        Ok(mut file) => {
-            file.write_all(new_brightness.round().to_string().as_bytes()).expect("Unable to write brightness.");
+    return Ok(String::new());
+}
+
+fn get_brightness(device: &str) -> Result<u32, Box<dyn std::error::Error>> {
+    return Ok(fs::read_to_string(format!("{}/brightness", device))?.trim().parse::<u32>()?);
+}
+
+fn get_max_brightness(device: &str) -> Result<u32, Box<dyn std::error::Error>> {
+    return Ok(fs::read_to_string(format!("{}/max_brightness", device))?.trim().parse::<u32>()?);
+}
+
+fn set_brightness_absolute_percent(device: &str, mut percentage: &u8) -> Result<(), Box<dyn std::error::Error>> {
+    if percentage > &100 {
+        eprintln!("Warning cannot set brightness higher then 100%, truncating to 100%");
+        percentage = &100;
+    }
+    let new_brightness: f32 = ( *percentage as f32 / 100.0 ) * get_max_brightness(device)? as f32;     
+
+    let mut brightness = File::create(format!("{}/brightness", device))?;
+    brightness.write(new_brightness.round().to_string().as_bytes())?;
+    brightness.flush()?;
+
+    return Ok(());
+}
+
+enum Sign {
+    Plus,
+    Minus,
+}
+
+fn set_brightness_relative_percent(device: &str, percentage: &u8, operation: &Sign) -> Result<(), Box<dyn std::error::Error>> {
+    let new_brightness_percent: f32;
+    let brightness: f32 = get_brightness(device)? as f32;
+    let max_brightness: f32 = get_max_brightness(device)? as f32;
+    
+    match operation {
+        Sign::Plus => {
+            new_brightness_percent = ( ( brightness / max_brightness ) * 100.00 ) + *percentage as f32;
         },
-        Err(e) => match e.kind() {
-            ErrorKind::NotFound => eprintln!("The file was not found."),
-            ErrorKind::PermissionDenied => eprintln!("Permission denied."),
-            e => eprintln!("An error occurred: {}", e),
-        }
+        Sign::Minus => {
+            new_brightness_percent = ( ( brightness / max_brightness ) * 100.00 ) - *percentage as f32;
+        },
     }
+    set_brightness_absolute_percent(device, &(new_brightness_percent as u8))?;
+
+    return Ok(());
 }
 
-fn check_backlight(backlight: &str) -> Result<String, String> {
-    let class: &Path = Path::new(&backlight);
-    let class_backlight: String = format!("/sys/class/backlight/{}", backlight);
-    let class_leds: String = format!("/sys/class/leds/{}", backlight);
-
-    if class.exists() && backlight.contains("/sys/class") {
+fn find_backlight(backlight: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let class_path: &Path = Path::new(&backlight);
+    if class_path.exists() && backlight.contains("/sys/class") {
         return Ok(backlight.to_string());
-    } else if Path::new(&class_backlight).exists() {
-        return Ok(class_backlight);
-    } else if Path::new(&class_leds).exists() {
-        return Ok(class_leds);
-    } else {
-        return Err(backlight.to_string());
     }
-}
-
-fn help() {
-    println!("usage: rslighty [--help] [--version] [--list] [--device DEVICE] [--get]");
-    println!("                [--get-steps] [--set PERCENT] [--inc PERCENT] [--dec PERCENT]");
-    return;
+    let led_path = format!("/sys/class/leds/{}", backlight);
+    if Path::new(&led_path).exists() {
+        return Ok(led_path);
+    }
+    let backlight_path = format!("/sys/class/backlight/{}", backlight);
+    if Path::new(&backlight_path).exists() {
+        return Ok(backlight_path);
+    }
+    
+    return Err("Device not found".into());
 }
 
 fn version() {
@@ -76,66 +77,46 @@ fn version() {
 }
 
 fn main() {    
-    let args: Vec<String> = env::args().collect();
+    let matches = command!()
+    .arg(Arg::new("list")
+            .long("list")
+            .help("lists available backlight and led devices")
+            .action(ArgAction::SetTrue))
+    .arg(Arg::new("device")
+            .long("device")
+            .help("Device to adjust brightness of")
+            .value_name("device")
+            .action(ArgAction::Set))
+    .arg(Arg::new("get")
+            .long("get")
+            .help("Get current brightness of selected device")
+            .action(ArgAction::SetTrue))
+    .arg(Arg::new("get-steps")
+            .long("get-steps")
+            .help("Get brightness steps of selected device")
+            .action(ArgAction::SetTrue))
+    .arg(Arg::new("set")
+            .long("set")
+            .help("Set brightness in percent of selected device")
+            .value_name("percent")
+            .action(ArgAction::Set))
+    .arg(Arg::new("inc")
+            .long("inc")
+            .help("Increase brightness in percent of selected device")
+            .value_name("percent")
+            .action(ArgAction::Set))
+    .arg(Arg::new("dec")
+            .long("dec")
+            .help("Decrease brightness in percent of selected device")
+            .value_name("percent")
+            .action(ArgAction::Set))
+    .get_matches();
+    
 
-    if args.len() < 2 {
-        help();
-        return;
-    }
 
-    println!("Arguments: {:?}", args);
-    let mut percent: Option<String> = None;
-    let mut operation: Option<String> = None;
-    let mut device: Option<String> = None;
-    let mut get_percent: bool = false;
-    let mut get_steps: bool = false;
 
-    let mut i = 1;
-    while i < args.len() {
-        match args[i].as_str() {
-            "--help" => help(),
-            "-h" => help(),
-            "--version" => version(),
-            "-v" => version(),
-            "--list" => list_devices(),
-            "--device" => {
-                if i + 1 < args.len() {
-                    //flag = Some(true);
-                    device = Some(args[i + 1].clone());
-                    i += 1;
-                } else {
-                    println!("Error: '--flag' requires a value");
-                    return;
-                }
-            }
-            _ => {
-                println!("Unknown argument: {}", args[i]);
-                return;
-            }
-        }
-        i += 1;
-    }
-    match device {
-        Some(s) => println!("{s}"),
-        None => println!("None"), 
-    };
-        //match flag {
-        //    Some(true) => {
-        //        println!("Flag is set, with value: {}", value.unwrap());
-        //    }
-        //    None => {
-        //        println!("Flag is not set.");
-        //    }
-        //}
-
-    //match check_backlight("apple-panel-bl") {
-    //    Ok(backlight) => {
-    //        //if percentage > &100 {
-    //        //    eprintln!("Cannot set display brightness higher then 100%.");
-    //        //    percentage = &100;
-    //        //}
-    //        set_brightness_absolute_percent(&backlight, &100);
-    //    },
-    //    Err(e) => eprintln!("Error: {}", e),
+    //match set_brightness_relative_percent("/sys/class/backlight/apple-panel-bl", &10, &Sign::Plus) {
+    //    Ok(t) => t,
+    //    Err(e) => eprintln!("{}", e)
     //}
 }
